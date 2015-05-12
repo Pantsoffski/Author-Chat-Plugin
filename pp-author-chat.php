b7b32380402293d3994909454cfc08d2334f9fb1
Pantsoffski <piotr.pesta@gmail.com>
2015-05-12 21:53:09 +0200

    developing

--- pp-author-chat.php
+++ pp-author-chat.php
@@ -10,13 +10,23 @@ License: GPL12
 */
 
 add_action('admin_menu', 'author_chat_setup_menu');
+add_action( 'wp_dashboard_setup', 'wp_dashboard_author_chat' );
 
-function test_plugin_setup_menu(){
-	add_menu_page( 'Author Chat Options', 'Author Chat', 'manage_options', 'author-chat', 'author_chat_setup_init' );
+function author_chat_setup_menu(){
+	add_menu_page( 'Author Chat Options', 'Author Chat', 'manage_options', 'author-chat-options', 'author_chat_setup_init' );
+	add_dashboard_page('Author Chat Window', 'Author Chat', 'read', 'author-chat', 'author_chat');
 }
 
-function author_chat_setup_init() {
-	include('admin.php');
+function wp_dashboard_author_chat(){
+	wp_add_dashboard_widget('author-chat-widget', 'Author Chat', 'author_chat');
+}
+
+function author_chat_setup_init(){
+	include('admin-menu.php');
+}
+
+function author_chat(){
+	echo "Admin Page Test";
 }
 
 ?>
