f23a4813fccbbc97ff6ef2a7947864048f1d467e
Pantsoffski <piotr.pesta@gmail.com>
2015-05-13 23:25:36 +0200

    developing

---
+++ chat.js
@@ -0,0 +1,87 @@
+/* 
+Created by: Kenrick Beckett
+
+Name: Chat Engine
+*/
+
+var instanse = false;
+var state;
+var mes;
+var file;
+
+function Chat () {
+    this.update = updateChat;
+    this.send = sendChat;
+	this.getState = getStateOfChat;
+}
+
+//gets the state of the chat
+function getStateOfChat(){
+	if(!instanse){
+		 instanse = true;
+		 jQuery.ajax({
+			   type: "POST",
+			   url: "http://localhost/www/WP/wp-content/plugins/author-chat/process.php",
+			   data: {  
+			   			'function': 'getState',
+						'file': file
+						},
+			   dataType: "json",
+			
+			   success: function(data){
+				   state = data.state;
+				   instanse = false;
+			   },
+			});
+	}	 
+}
+
+//Updates the chat
+function updateChat(){
+	 if(!instanse){
+		 instanse = true;
+	     jQuery.ajax({
+			   type: "POST",
+			   url: "http://localhost/www/WP/wp-content/plugins/author-chat/process.php",
+			   data: {  
+			   			'function': 'update',
+						'state': state,
+						'file': file
+						},
+			   dataType: "json",
+			   success: function(data){
+				   if(data.text){
+						for (var i = 0; i < data.text.length; i++) {
+                            jQuery('#chat-area').append(jQuery("<p>"+ data.text[i] +"</p>"));
+                        }								  
+				   }
+				   document.getElementById('chat-area').scrollTop = document.getElementById('chat-area').scrollHeight;
+				   instanse = false;
+				   state = data.state;
+			   },
+			});
+	 }
+	 else {
+		 setTimeout(updateChat, 1500);
+	 }
+}
+
+//send the message
+function sendChat(message, nickname)
+{       
+    updateChat();
+     jQuery.ajax({
+		   type: "POST",
+		   url: "http://localhost/www/WP/wp-content/plugins/author-chat/process.php",
+		   data: {  
+		   			'function': 'send',
+					'message': message,
+					'nickname': nickname,
+					'file': file
+				 },
+		   dataType: "json",
+		   success: function(data){
+			   updateChat();
+		   },
+		});
+}
--- pp-author-chat.php
+++ pp-author-chat.php
@@ -11,6 +11,13 @@ License: GPL12
 
 add_action('admin_menu', 'author_chat_setup_menu');
 add_action( 'wp_dashboard_setup', 'wp_dashboard_author_chat' );
+add_action( 'admin_enqueue_scripts', 'scripts_admin_chat' );
+
+function scripts_admin_chat(){
+	wp_register_script('chat-script', plugins_url('chat.js', __FILE__ ));
+	wp_enqueue_script('chat-script');
+	wp_enqueue_style('author-chat-style', plugins_url('author-chat-style.css', __FILE__));
+}
 
 function author_chat_setup_menu(){
 	add_menu_page( 'Author Chat Options', 'Author Chat', 'manage_options', 'author-chat-options', 'author_chat_setup_init' );
@@ -26,7 +33,99 @@ function author_chat_setup_init(){
 }
 
 function author_chat(){
-	echo "Admin Page Test";
+	?>
+	
+
+
+<frame onload="setInterval('chat.update()', 1000)">
+
+    <div id="page-wrap">
+    
+        <h2>jQuery/PHP Chat</h2>
+        
+        <p id="name-area"></p>
+        
+        <div id="chat-wrap"><div id="chat-area"></div></div>
+        
+        <form id="send-message-area">
+            <p>Your message: </p>
+            <textarea id="sendie" maxlength = '100' ></textarea>
+        </form>
+    
+    </div>
+
+</frame>
+
+</html>
+	
+    <script type="text/javascript">
+    
+        // ask user for name with popup prompt    
+        var name = prompt("Enter your chat name:", "Guest");
+        
+        // default name is 'Guest'
+    	if (!name || name === ' ') {
+    	   name = "Guest";	
+    	}
+    	
+    	// strip tags
+    	name = name.replace(/(<([^>]+)>)/ig,"");
+    	
+    	// display name on page
+    	jQuery("#name-area").html("You are: <span>" + name + "</span>");
+    	
+    	// kick off chat
+        var chat =  new Chat();
+    	jQuery(function() {
+    	
+    		 chat.getState(); 
+    		 
+    		 // watch textarea for key presses
+             jQuery("#sendie").keydown(function(event) {  
+             
+                 var key = event.which;  
+           
+                 //all keys including return.  
+                 if (key >= 33) {
+                   
+                     var maxLength = jQuery(this).attr("maxlength");  
+                     var length = this.value.length;  
+                     
+                     // don't allow new content if length is maxed out
+                     if (length >= maxLength) {  
+                         event.preventDefault();  
+                     }  
+                  }  
+    		 																																																});
+    		 // watch textarea for release of key press
+    		 jQuery('#sendie').keyup(function(e) {	
+    		 					 
+    			  if (e.keyCode == 13) { 
+    			  
+                    var text = jQuery(this).val();
+    				var maxLength = jQuery(this).attr("maxlength");  
+                    var length = text.length; 
+                     
+                    // send 
+                    if (length <= maxLength + 1) { 
+                     
+    			        chat.send(text, name);	
+    			        jQuery(this).val("");
+    			        
+                    } else {
+                    
+    					jQuery(this).val(text.substring(0, maxLength));
+    					
+    				}	
+    				
+    				
+    			  }
+             });
+            
+    	});
+    </script>
+	
+	<?php
 }
 
 ?>
